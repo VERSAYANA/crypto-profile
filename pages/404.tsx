4a6610965307153092b3fd1aebe37b8f9f4117bc
Ramtin Khalatbari <VERSAYANA@outlook.com>
2022-12-31 19:43:00 +0330

    Add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center gap-y-4">
+      <h1 className="text-7xl font-bold">404</h1>
+      <h2 className="text-xl opacity-90">Page Not Found</h2>
+    </div>
+  )
+}
