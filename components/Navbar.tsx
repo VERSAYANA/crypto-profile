1e3f56a8f431ba74f1e7ffd25c73ea0cd1911002
Ramtin Khalatbari <VERSAYANA@outlook.com>
2022-12-31 18:57:03 +0330

    Add landing page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,10 +3,13 @@ import Link from 'next/link'
 import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react'
 import { Database } from '../utils/database.types'
 import Avatar from './Avatar'
-import { ChevronDown, Moon, Sun } from 'react-feather'
+import { ChevronDown, Home, Moon, Sun } from 'react-feather'
 import { themeChange } from 'theme-change'
+import { themes } from '../utils/constants'
 
 type Profiles = Database['public']['Tables']['profiles']['Row']
+
+type Theme = 'dark' | 'light' | 'system' | null
 
 function Navbar() {
   const supabase = useSupabaseClient<Database>()
@@ -61,7 +64,7 @@ function Navbar() {
   )
 
   const userDropDown = (
-    <div className="dropdown-end dropdown">
+    <div className="dropdown dropdown-bottom">
       <label tabIndex={0} className="btn-ghost btn">
         <div className="flex items-center gap-4">
           <ChevronDown />
@@ -97,26 +100,78 @@ function Navbar() {
   }
 
   return (
-    <div className="navbar sticky top-0 z-10 items-center justify-center  bg-base-100 shadow">
+    <div className="navbar sticky top-0 z-10 items-center justify-center bg-base-100 px-2 shadow md:px-4">
       <div className="container flex w-full justify-between">
-        <div>{/* <button className="btn-accent btn">Save</button> */}</div>
-        <div className="flex justify-end gap-2">
-          <button
-            className="btn-ghost btn-square btn"
-            data-set-theme="light"
-            data-act-class="hidden"
-          >
-            <Sun className="" size={24} />
-          </button>
-          <button
-            className="btn-ghost btn-square btn"
-            data-act-class="hidden"
-            data-set-theme="dark"
-          >
-            <Moon className="" size={24} />
-          </button>
-          {topRight}
+        <div className="flex gap-2">
+          <Link href={'/'} className="btn-ghost btn-square btn">
+            <Home size={24} />
+          </Link>
+
+          <div title="Change Theme" className="dropdown">
+            <div tabIndex={0} className={`btn-ghost btn gap-1 normal-case`}>
+              <svg
+                width="20"
+                height="20"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-5 w-5 stroke-current md:h-6 md:w-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+                />
+              </svg>
+              <span className="hidden md:inline">Theme</span>
+              <svg
+                width="12px"
+                height="12px"
+                className="ml-1 hidden h-3 w-3 fill-current opacity-60 sm:inline-block"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 2048 2048"
+              >
+                <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z" />
+              </svg>
+
+              <div
+                className={`dropdown-content rounded-t-box rounded-b-box top-px mt-16 h-[70vh] max-h-96 w-40 overflow-y-auto bg-base-200 text-base-content shadow-2xl md:w-52`}
+              >
+                <div className="grid grid-cols-1 gap-3 p-3" tabIndex={0}>
+                  {themes.map((theme) => (
+                    <div
+                      key={theme.id}
+                      className="overflow-hidden rounded-lg outline outline-2 outline-offset-2 outline-base-content"
+                      data-set-theme={theme.id}
+                      data-act-class="outline"
+                    >
+                      <div
+                        data-theme={theme.id}
+                        className="w-full cursor-pointer bg-base-100 font-sans text-base-content"
+                      >
+                        <div className="grid grid-cols-5 grid-rows-3">
+                          <div className="col-span-5 row-span-3 row-start-1 flex gap-1 py-3 px-4">
+                            <div className="flex-grow text-sm font-bold">
+                              {theme.id}
+                            </div>
+                            {/* <div className="flex flex-shrink-0 flex-wrap gap-1">
+                              <div className="w-2 rounded bg-primary" />
+                              <div className="w-2 rounded bg-secondary" />
+                              <div className="w-2 rounded bg-accent" />
+                              <div className="w-2 rounded bg-neutral" />
+                            </div> */}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        <div className="flex justify-end gap-1 md:gap-2">{topRight}</div>
       </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import Account from '../components/Account'
+import Link from 'next/link'
 
 export default function Home() {
   const session = useSession()
@@ -17,11 +18,42 @@ export default function Home() {
         <link rel="icon" href="/btc.svg" />
         <link rel="icon" href="/btc.png" />
       </Head>
-      <div className="container mx-auto flex justify-center">
-        <div className="flex w-full max-w-3xl items-center justify-center p-4">
+      <div id="home" className="container mx-auto flex flex-1 justify-center">
+        <div className="flex w-full max-w-3xl flex-1 items-center justify-center p-3 md:p-4">
           {!session ? (
-            <div className="w-full">
-              <Auth
+            <div className="flex w-full flex-col items-center justify-center gap-8">
+              <h1 className="max-w-xl text-center text-3xl md:text-5xl">
+                Share your cryptocurrency addresses with ease
+              </h1>
+              <h2 className="text-center text-lg opacity-80 md:text-2xl">
+                Create a profile like{' '}
+                <Link target={'_blank'} href="/versayana">
+                  <span className="link">this</span>
+                </Link>{' '}
+                to receive cryptocurrency payments or donations.
+              </h2>
+              <div className="flex w-full flex-col items-center gap-y-4 px-4 md:p-0">
+                <div className="hidden w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-xl md:flex">
+                  <div className="flex">
+                    <span>send-me-crypto.vercel.app/</span>
+                    <span className="opacity-50">username</span>
+                  </div>
+                  <button className="btn-secondary btn ml-2 rounded-full normal-case">
+                    Create my page
+                  </button>
+                </div>
+
+                <div className="flex h-12 w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-base md:hidden">
+                  <div className="flex">
+                    <span>send-me-crypto.vercel.app/</span>
+                    <span className="opacity-50">username</span>
+                  </div>
+                </div>
+                <button className="btn-secondary btn w-full rounded-full normal-case md:hidden">
+                  Create my page
+                </button>
+              </div>
+              {/* <Auth
                 supabaseClient={supabase}
                 appearance={{
                   theme: ThemeSupa,
@@ -33,7 +65,7 @@ export default function Home() {
                   },
                 }}
                 theme="default"
-              />
+              /> */}
             </div>
           ) : (
             <Account session={session} />
