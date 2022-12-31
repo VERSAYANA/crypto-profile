a1a5cd7517aa7e13683fc23d2f1fe9169839b92d
Ramtin Khalatbari <VERSAYANA@outlook.com>
2022-12-31 12:31:38 +0330

    Add table view for profile

--- pages/[username].tsx
+++ pages/[username].tsx
@@ -3,15 +3,17 @@ import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { Database } from '../utils/database.types'
 import Avatar from '../components/Avatar'
-import { GitHub, Twitter, Link as LinkIcon } from 'react-feather'
+import { GitHub, Twitter, Link as LinkIcon, Copy } from 'react-feather'
 import { getValidUrlFromUsernameOrUrl } from '../utils/functions'
-import { coinsMap, orderCoins } from '../utils/constants'
+import { coins, coinsMap, orderCoins } from '../utils/constants'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { supabase as supabaseClient } from '../utils/supabase'
 import { ParsedUrlQuery } from 'querystring'
 import Head from 'next/head'
 import SingleNetworkAssetView from '../components/SingleNetworkAssetView'
 import MultipleNetworksAssetView from '../components/MultipleNetworksAssetView'
+import CopyToClipboard from 'react-copy-to-clipboard'
+import { QR } from '../components/Icons/QR'
 
 type Profiles = Database['public']['Tables']['profiles']['Row']
 
@@ -49,40 +51,137 @@ function Username({ profile }: Props) {
     return () => clearTimeout(timer)
   }, [toast])
 
-  let assets = null
+  let tableView = null
+
+  // let assets = null
 
   if (profile?.addresses) {
     const coins = orderCoins(profile.addresses)
 
-    assets = coins.map((value) => {
-      const { asset, addresses } = value
-      const coin = coinsMap.get(asset)!
+    tableView = coins.map(({ asset, addresses }) => {
+      const coinDetails = coinsMap.get(asset)!
+      // console.log(addresses)
 
-      if (coin?.networks.length <= 1) {
-        return (
-          <SingleNetworkAssetView
-            key={asset}
-            asset={asset}
-            network={addresses[0].network}
-            address={addresses[0].address}
-            activeQR={activeQR}
-            setActiveQR={setActiveQR}
-            setToast={setToast}
-          />
-        )
-      } else {
-        return (
-          <MultipleNetworksAssetView
-            key={asset}
-            asset={asset}
-            addresses={addresses}
-            activeQR={activeQR}
-            setActiveQR={setActiveQR}
-            setToast={setToast}
-          />
-        )
-      }
+      return (
+        <div
+          key={asset}
+          className="flex flex-col rounded-lg bg-base-200 px-3 py-1 md:py-2 md:px-4"
+        >
+          {addresses.map(({ network, address }, i) => {
+            return (
+              <div key={network + asset} className="flex h-14 items-center">
+                <div className="flex w-24 md:w-44">
+                  {i === 0 ? (
+                    <div className="flex items-center gap-2 md:gap-3">
+                      <img
+                        className="h-6 w-6 md:h-8 md:w-8"
+                        src={coinDetails.logo}
+                        alt={`${coinDetails.abbreviation} Logo`}
+                      />
+                      <div className="flex flex-col items-start justify-start text-xs md:text-sm">
+                        <span className="hidden md:flex">
+                          {coinDetails.name}
+                        </span>
+                        <span className="md:opacity-40">
+                          {coinDetails.abbreviation}
+                        </span>
+                      </div>
+                    </div>
+                  ) : (
+                    ''
+                  )}
+                </div>
+                <div className="flex w-16 md:w-20 lg:w-24 ">{network}</div>
+                <div className="flex w-full flex-1 overflow-x-hidden">
+                  <CopyToClipboard
+                    text={address}
+                    onCopy={() => {
+                      setToast({
+                        hidden: false,
+                        message: `${asset} address copied to clipboard`,
+                      })
+                    }}
+                  >
+                    <div className=" flex h-10 w-full cursor-pointer items-center rounded-lg bg-base-300 px-2 text-xs sm:text-sm md:px-4">
+                      {address}
+                    </div>
+                  </CopyToClipboard>
+                </div>
+
+                <div className="hidden items-center justify-between md:flex md:pl-2">
+                  <CopyToClipboard
+                    text={address}
+                    onCopy={() => {
+                      setToast({
+                        hidden: false,
+                        message: `${asset} address copied to clipboard`,
+                      })
+                    }}
+                  >
+                    <button className="btn-ghost btn-square btn">
+                      <Copy size={24} />
+                    </button>
+                  </CopyToClipboard>
+                  <button
+                    onClick={() => {
+                      if (
+                        activeQR.asset === asset &&
+                        activeQR.network === network
+                      ) {
+                        setActiveQR({
+                          address: '',
+                          network: '',
+                          asset: '',
+                        })
+                      } else {
+                        setActiveQR({
+                          address,
+                          network,
+                          asset,
+                        })
+                      }
+                    }}
+                    className="btn-ghost btn-square btn "
+                  >
+                    <QR width="24" height="24" />
+                  </button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      )
     })
+
+    // assets = coins.map(({ asset, addresses }) => {
+    //   const coin = coinsMap.get(asset)!
+    //   // console.log(addresses)
+
+    //   if (coin?.networks.length <= 1) {
+    //     return (
+    //       <SingleNetworkAssetView
+    //         key={asset}
+    //         asset={asset}
+    //         network={addresses[0].network}
+    //         address={addresses[0].address}
+    //         activeQR={activeQR}
+    //         setActiveQR={setActiveQR}
+    //         setToast={setToast}
+    //       />
+    //     )
+    //   } else {
+    //     return (
+    //       <MultipleNetworksAssetView
+    //         key={asset}
+    //         asset={asset}
+    //         addresses={addresses}
+    //         activeQR={activeQR}
+    //         setActiveQR={setActiveQR}
+    //         setToast={setToast}
+    //       />
+    //     )
+    //   }
+    // })
   }
 
   return (
@@ -96,7 +195,7 @@ function Username({ profile }: Props) {
         <link rel="icon" href="/btc.png" />
       </Head>
       <div className="container mx-auto flex justify-center">
-        <div className="flex w-full max-w-2xl flex-col items-center justify-center p-4">
+        <div className="flex w-full max-w-4xl flex-col items-center justify-center p-4">
           <div className="flex w-full flex-col">
             <div className="mt-6">
               <Avatar url={profile?.avatar_url || ''} />
@@ -157,7 +256,15 @@ function Username({ profile }: Props) {
                 </div>
               </div>
             </div>
-            <div className="flex flex-col gap-4">{assets}</div>
+            {/* <div className="flex flex-col gap-4 md:hidden">{assets}</div> */}
+            <div className="flex flex-col gap-y-3 text-xs sm:text-sm md:gap-y-4">
+              <div className="flex px-3 font-bold md:px-4">
+                <div className="flex w-24 md:w-44 lg:w-48">Asset</div>
+                <div className="flex w-16 md:w-24">Network</div>
+                <div className="flex flex-1">Address</div>
+              </div>
+              {tableView}
+            </div>
           </div>
         </div>
       </div>
