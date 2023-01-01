8cc095ae546bf9132d22b97b6f2d300e429abeb9
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-01-01 22:13:12 +0330

    Add more shadows

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,20 +29,20 @@ export default function Home() {
                 to receive cryptocurrency payments or donations.
               </h2>
               <div className="flex w-full flex-col items-center gap-y-4 px-4 md:p-0">
-                <div className="hidden w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-xl drop-shadow md:flex">
+                <div className="hidden w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-xl drop-shadow-md md:flex">
                   <div className="flex">
                     <span>send-me-crypto.vercel.app/</span>
                     <span className="opacity-50">username</span>
                   </div>
                   <Link
                     href={'/auth'}
-                    className="btn-secondary btn ml-2 rounded-full normal-case"
+                    className="btn-secondary btn ml-2 rounded-full normal-case drop-shadow-md"
                   >
                     Create my page
                   </Link>
                 </div>
 
-                <div className="flex h-12 w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-base md:hidden">
+                <div className="flex h-12 w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-base drop-shadow-md md:hidden">
                   <div className="flex">
                     <span>send-me-crypto.vercel.app/</span>
                     <span className="opacity-50">username</span>
@@ -50,7 +50,7 @@ export default function Home() {
                 </div>
                 <Link
                   href={'/auth'}
-                  className="btn-secondary btn w-full rounded-full normal-case md:hidden"
+                  className="btn-secondary btn w-full rounded-full normal-case drop-shadow-md md:hidden"
                 >
                   Create my page
                 </Link>
