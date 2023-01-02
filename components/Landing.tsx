c99b3ccc56c053833805c7705ff56dae665aff53
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-01-02 12:59:45 +0330

    Extract landing page to a separate component

---
+++ components/Landing.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link'
+
+function Landing() {
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-8 md:gap-9">
+      <h1 className="max-w-xl text-center text-3xl md:text-5xl">
+        Share all of your cryptocurrency addresses in one page
+      </h1>
+      <h2 className="text-center text-lg opacity-80 md:text-2xl">
+        Create a profile like{' '}
+        <Link target={'_blank'} href="/versayana">
+          <span className="link">this</span>
+        </Link>{' '}
+        to receive cryptocurrency payments or donations.
+      </h2>
+      <div className="flex w-full flex-col items-center gap-y-4 px-4 md:p-0">
+        <div className="hidden w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-xl drop-shadow-md md:flex">
+          <div className="flex">
+            <span>send-me-crypto.vercel.app/</span>
+            <span className="opacity-50">username</span>
+          </div>
+          <Link
+            href={'/auth'}
+            className="btn-secondary btn ml-2 rounded-full normal-case drop-shadow-md"
+          >
+            Create my page
+          </Link>
+        </div>
+
+        <div className="flex h-12 w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-base drop-shadow-md md:hidden">
+          <div className="flex">
+            <span>send-me-crypto.vercel.app/</span>
+            <span className="opacity-50">username</span>
+          </div>
+        </div>
+        <Link
+          href={'/auth'}
+          className="btn-secondary btn w-full rounded-full normal-case drop-shadow-md md:hidden"
+        >
+          Create my page
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Landing
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import Account from '../components/Account'
 import Link from 'next/link'
+import Landing from '../components/Landing'
 
 export default function Home() {
   const session = useSession()
@@ -16,49 +17,7 @@ export default function Home() {
       </Head>
       <div id="home" className="container mx-auto flex flex-1 justify-center">
         <div className="flex w-full max-w-3xl flex-1 items-center justify-center p-4">
-          {!session ? (
-            <div className="flex w-full flex-col items-center justify-center gap-8">
-              <h1 className="max-w-xl text-center text-3xl md:text-5xl">
-                Share all of your cryptocurrency addresses in one page
-              </h1>
-              <h2 className="text-center text-lg opacity-80 md:text-2xl">
-                Create a profile like{' '}
-                <Link target={'_blank'} href="/versayana">
-                  <span className="link">this</span>
-                </Link>{' '}
-                to receive cryptocurrency payments or donations.
-              </h2>
-              <div className="flex w-full flex-col items-center gap-y-4 px-4 md:p-0">
-                <div className="hidden w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-xl drop-shadow-md md:flex">
-                  <div className="flex">
-                    <span>send-me-crypto.vercel.app/</span>
-                    <span className="opacity-50">username</span>
-                  </div>
-                  <Link
-                    href={'/auth'}
-                    className="btn-secondary btn ml-2 rounded-full normal-case drop-shadow-md"
-                  >
-                    Create my page
-                  </Link>
-                </div>
-
-                <div className="flex h-12 w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-base drop-shadow-md md:hidden">
-                  <div className="flex">
-                    <span>send-me-crypto.vercel.app/</span>
-                    <span className="opacity-50">username</span>
-                  </div>
-                </div>
-                <Link
-                  href={'/auth'}
-                  className="btn-secondary btn w-full rounded-full normal-case drop-shadow-md md:hidden"
-                >
-                  Create my page
-                </Link>
-              </div>
-            </div>
-          ) : (
-            <Account session={session} />
-          )}
+          {!session ? <Landing /> : <Account session={session} />}
         </div>
       </div>
     </>
