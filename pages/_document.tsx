79176dc43b978b57e703c39f331fc7cb4bf1c3f3
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-01-02 17:55:49 +0330

    Add twitter meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,15 +10,34 @@ export default function Document() {
           name="description"
           content="Share all of your cryptocurrency addresses in one page"
         />
+        <meta
+          property="og:title"
+          content="Make your crypto profile"
+          key="title"
+        />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://send-me-crypto.vercel.app" />
+        <meta property="og:url" content="https://cryptoprofile.vercel.app/" />
         <meta
           property="og:description"
           content="Share all of your cryptocurrency addresses in one page."
         />
         <meta
           property="og:image"
-          content="https://send-me-crypto.vercel.app/mycryptoprofile.png"
+          content="https://cryptoprofile.vercel.app//mycryptoprofile.png"
+        />
+
+        <meta
+          property="twitter:url"
+          content="https://cryptoprofile.vercel.app/"
+        />
+        <meta property="twitter:title" content="Make your crypto profile" />
+        <meta
+          property="twitter:description"
+          content="Share all of your cryptocurrency addresses in one page."
+        />
+        <meta
+          property="twitter:image"
+          content="https://cryptoprofile.vercel.app//mycryptoprofile.png"
         />
       </Head>
       <body>
