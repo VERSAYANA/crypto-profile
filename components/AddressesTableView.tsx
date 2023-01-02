3d48885f79e24b7af7a9d15bacb17ea159fa414d
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-01-02 13:48:45 +0330

    Extract AddressesTableView to a separate component

---
+++ components/AddressesTableView.tsx
@@ -0,0 +1,159 @@
+/* eslint-disable @next/next/no-img-element */
+import { QRCodeSVG } from 'qrcode.react'
+import { Dispatch, SetStateAction, useState } from 'react'
+import CopyToClipboard from 'react-copy-to-clipboard'
+import { Copy } from 'react-feather'
+import { coinsMap } from '../utils/constants'
+import { QR } from './Icons/QR'
+
+type Props = {
+  assetAddresses: {
+    asset: string
+    addresses: {
+      network: string
+      address: string
+    }[]
+  }[]
+  setToast: Dispatch<
+    SetStateAction<{
+      hidden: boolean
+      message: string
+    }>
+  >
+}
+
+function AddressesTableView({ assetAddresses, setToast }: Props) {
+  const [activeQR, setActiveQR] = useState({
+    asset: '',
+    network: '',
+    address: '',
+  })
+
+  return (
+    <div className="flex flex-col gap-y-3 text-xs sm:text-sm md:gap-y-4">
+      <div className="flex px-3 font-bold md:px-4">
+        <div className="flex w-24 md:w-44 lg:w-48">Asset</div>
+        <div className="flex w-16 md:w-24">Network</div>
+        <div className="flex flex-1">Address</div>
+      </div>
+
+      {assetAddresses.map(({ asset, addresses }) => {
+        const coinDetails = coinsMap.get(asset)!
+
+        if (addresses.length > 0) {
+          return (
+            <div
+              key={asset}
+              className="flex flex-col rounded-lg bg-base-200 px-3 py-1 drop-shadow-sm md:py-2 md:px-4"
+            >
+              {addresses.map(({ network, address }, i) => {
+                return (
+                  <div key={network + asset} className="flex flex-col">
+                    <div
+                      className={`flex h-14 items-center ${
+                        activeQR.asset === asset && activeQR.network === network
+                          ? 'font-bold'
+                          : ''
+                      }`}
+                    >
+                      <div className="flex w-24 md:w-44 lg:w-48">
+                        {i === 0 ? (
+                          <div
+                            className={`flex items-center gap-2 md:gap-3 ${
+                              activeQR.asset === asset ? 'font-bold' : ''
+                            }`}
+                          >
+                            <img
+                              className="h-6 w-6 md:h-8 md:w-8"
+                              src={coinDetails.logo}
+                              alt={`${coinDetails.abbreviation} Logo`}
+                            />
+                            <div className="flex flex-col items-start justify-start text-xs md:text-sm">
+                              <span className="hidden md:flex">
+                                {coinDetails.name}
+                              </span>
+                              <span className="md:opacity-40">
+                                {coinDetails.abbreviation}
+                              </span>
+                            </div>
+                          </div>
+                        ) : (
+                          ''
+                        )}
+                      </div>
+                      <div className="flex w-16 md:w-20 lg:w-24 ">
+                        {network}
+                      </div>
+                      <div className="flex w-full flex-1 overflow-x-hidden">
+                        <CopyToClipboard
+                          text={address}
+                          onCopy={() => {
+                            setToast({
+                              hidden: false,
+                              message: `${asset} address copied to clipboard`,
+                            })
+                          }}
+                        >
+                          <div className=" flex h-10 w-full cursor-pointer items-center rounded-lg bg-base-300 px-2 text-xs sm:text-sm md:px-4">
+                            {address}
+                          </div>
+                        </CopyToClipboard>
+                      </div>
+
+                      <div className="hidden items-center justify-between md:flex md:pl-2">
+                        <CopyToClipboard
+                          text={address}
+                          onCopy={() => {
+                            setToast({
+                              hidden: false,
+                              message: `${asset} address copied to clipboard`,
+                            })
+                          }}
+                        >
+                          <button className="btn-ghost btn-square btn">
+                            <Copy size={24} />
+                          </button>
+                        </CopyToClipboard>
+                        <button
+                          onClick={() => {
+                            if (
+                              activeQR.asset === asset &&
+                              activeQR.network === network
+                            ) {
+                              setActiveQR({
+                                address: '',
+                                network: '',
+                                asset: '',
+                              })
+                            } else {
+                              setActiveQR({
+                                address,
+                                network,
+                                asset,
+                              })
+                            }
+                          }}
+                          className="btn-ghost btn-square btn "
+                        >
+                          <QR width="24" height="24" />
+                        </button>
+                      </div>
+                    </div>
+                    {activeQR.asset === asset &&
+                    activeQR.network === network ? (
+                      <div className="flex items-center justify-center pt-2 pb-4">
+                        <QRCodeSVG value={activeQR.address} />
+                      </div>
+                    ) : null}
+                  </div>
+                )
+              })}
+            </div>
+          )
+        }
+      })}
+    </div>
+  )
+}
+
+export default AddressesTableView
--- pages/[username].tsx
+++ pages/[username].tsx
@@ -5,14 +5,11 @@ import { Database } from '../utils/database.types'
 import Avatar from '../components/Avatar'
 import { GitHub, Twitter, Link as LinkIcon, Copy } from 'react-feather'
 import { getValidUrlFromUsernameOrUrl, orderCoins } from '../utils/functions'
-import { coinsMap } from '../utils/constants'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { supabase as supabaseClient } from '../utils/supabase'
 import { ParsedUrlQuery } from 'querystring'
 import Head from 'next/head'
-import CopyToClipboard from 'react-copy-to-clipboard'
-import { QR } from '../components/Icons/QR'
-import { QRCodeSVG } from 'qrcode.react'
+import AddressesTableView from '../components/AddressesTableView'
 
 type Profiles = Database['public']['Tables']['profiles']['Row']
 
@@ -25,6 +22,7 @@ type Props = {
 }
 
 function Username({ profile }: Props) {
+  // console.log(profile)
   const router = useRouter()
   const [toast, setToast] = useState({
     hidden: true,
@@ -49,157 +47,6 @@ function Username({ profile }: Props) {
     )
     return () => clearTimeout(timer)
   }, [toast])
-
-  let tableView = null
-
-  // let assets = null
-
-  if (profile?.addresses) {
-    const coins = orderCoins(profile.addresses)
-
-    tableView = coins.map(({ asset, addresses }) => {
-      const coinDetails = coinsMap.get(asset)!
-
-      if (addresses.length > 0) {
-        return (
-          <div
-            key={asset}
-            className="flex flex-col rounded-lg bg-base-200 px-3 py-1 drop-shadow-sm md:py-2 md:px-4"
-          >
-            {addresses.map(({ network, address }, i) => {
-              return (
-                <div key={network + asset} className="flex flex-col">
-                  <div
-                    className={`flex h-14 items-center ${
-                      activeQR.asset === asset && activeQR.network === network
-                        ? 'font-bold'
-                        : ''
-                    }`}
-                  >
-                    <div className="flex w-24 md:w-44 lg:w-48">
-                      {i === 0 ? (
-                        <div
-                          className={`flex items-center gap-2 md:gap-3 ${
-                            activeQR.asset === asset ? 'font-bold' : ''
-                          }`}
-                        >
-                          <img
-                            className="h-6 w-6 md:h-8 md:w-8"
-                            src={coinDetails.logo}
-                            alt={`${coinDetails.abbreviation} Logo`}
-                          />
-                          <div className="flex flex-col items-start justify-start text-xs md:text-sm">
-                            <span className="hidden md:flex">
-                              {coinDetails.name}
-                            </span>
-                            <span className="md:opacity-40">
-                              {coinDetails.abbreviation}
-                            </span>
-                          </div>
-                        </div>
-                      ) : (
-                        ''
-                      )}
-                    </div>
-                    <div className="flex w-16 md:w-20 lg:w-24 ">{network}</div>
-                    <div className="flex w-full flex-1 overflow-x-hidden">
-                      <CopyToClipboard
-                        text={address}
-                        onCopy={() => {
-                          setToast({
-                            hidden: false,
-                            message: `${asset} address copied to clipboard`,
-                          })
-                        }}
-                      >
-                        <div className=" flex h-10 w-full cursor-pointer items-center rounded-lg bg-base-300 px-2 text-xs sm:text-sm md:px-4">
-                          {address}
-                        </div>
-                      </CopyToClipboard>
-                    </div>
-
-                    <div className="hidden items-center justify-between md:flex md:pl-2">
-                      <CopyToClipboard
-                        text={address}
-                        onCopy={() => {
-                          setToast({
-                            hidden: false,
-                            message: `${asset} address copied to clipboard`,
-                          })
-                        }}
-                      >
-                        <button className="btn-ghost btn-square btn">
-                          <Copy size={24} />
-                        </button>
-                      </CopyToClipboard>
-                      <button
-                        onClick={() => {
-                          if (
-                            activeQR.asset === asset &&
-                            activeQR.network === network
-                          ) {
-                            setActiveQR({
-                              address: '',
-                              network: '',
-                              asset: '',
-                            })
-                          } else {
-                            setActiveQR({
-                              address,
-                              network,
-                              asset,
-                            })
-                          }
-                        }}
-                        className="btn-ghost btn-square btn "
-                      >
-                        <QR width="24" height="24" />
-                      </button>
-                    </div>
-                  </div>
-                  {activeQR.asset === asset && activeQR.network === network ? (
-                    <div className="flex items-center justify-center pt-2 pb-4">
-                      <QRCodeSVG value={activeQR.address} />
-                    </div>
-                  ) : null}
-                </div>
-              )
-            })}
-          </div>
-        )
-      }
-    })
-
-    // assets = coins.map(({ asset, addresses }) => {
-    //   const coin = coinsMap.get(asset)!
-    //   // console.log(addresses)
-
-    //   if (coin?.networks.length <= 1) {
-    //     return (
-    //       <SingleNetworkAssetView
-    //         key={asset}
-    //         asset={asset}
-    //         network={addresses[0].network}
-    //         address={addresses[0].address}
-    //         activeQR={activeQR}
-    //         setActiveQR={setActiveQR}
-    //         setToast={setToast}
-    //       />
-    //     )
-    //   } else {
-    //     return (
-    //       <MultipleNetworksAssetView
-    //         key={asset}
-    //         asset={asset}
-    //         addresses={addresses}
-    //         activeQR={activeQR}
-    //         setActiveQR={setActiveQR}
-    //         setToast={setToast}
-    //       />
-    //     )
-    //   }
-    // })
-  }
 
   return (
     <>
@@ -279,15 +126,12 @@ function Username({ profile }: Props) {
                 </div>
               </div>
             </div>
-            {/* <div className="flex flex-col gap-4 md:hidden">{assets}</div> */}
-            <div className="flex flex-col gap-y-3 text-xs sm:text-sm md:gap-y-4">
-              <div className="flex px-3 font-bold md:px-4">
-                <div className="flex w-24 md:w-44 lg:w-48">Asset</div>
-                <div className="flex w-16 md:w-24">Network</div>
-                <div className="flex flex-1">Address</div>
-              </div>
-              {tableView}
-            </div>
+            {profile?.addresses ? (
+              <AddressesTableView
+                assetAddresses={orderCoins(profile.addresses)}
+                setToast={setToast}
+              />
+            ) : null}
           </div>
         </div>
       </div>
