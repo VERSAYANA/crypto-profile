67656510c763484e60bc953ef727c9dc3ffe79c9
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-01-02 13:20:01 +0330

    Make the link to auth more clear

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -14,25 +14,28 @@ function Landing() {
         to receive cryptocurrency payments or donations.
       </h2>
       <div className="flex w-full flex-col items-center gap-y-4 px-4 md:p-0">
-        <div className="hidden w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-xl drop-shadow-md md:flex">
+        <Link
+          href={'/auth'}
+          className="hidden w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-xl drop-shadow-md md:flex"
+        >
           <div className="flex">
             <span>send-me-crypto.vercel.app/</span>
             <span className="opacity-50">username</span>
           </div>
-          <Link
-            href={'/auth'}
-            className="btn-secondary btn ml-2 rounded-full normal-case drop-shadow-md"
-          >
+          <button className="btn-secondary btn ml-2 rounded-full normal-case drop-shadow-md">
             Create my page
-          </Link>
-        </div>
+          </button>
+        </Link>
 
-        <div className="flex h-12 w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-base drop-shadow-md md:hidden">
+        <Link
+          href={'/auth'}
+          className="flex h-12 w-full max-w-xl items-center justify-between rounded-full border border-base-200 bg-base-200 py-2 pl-4 pr-2 text-base drop-shadow-md md:hidden"
+        >
           <div className="flex">
             <span>send-me-crypto.vercel.app/</span>
             <span className="opacity-50">username</span>
           </div>
-        </div>
+        </Link>
         <Link
           href={'/auth'}
           className="btn-secondary btn w-full rounded-full normal-case drop-shadow-md md:hidden"
